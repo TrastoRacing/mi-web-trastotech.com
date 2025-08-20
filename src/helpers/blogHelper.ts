ac209fa6fb590073782c66fa7a0347c5952f3e4a
Omar Santos <contacto@omarsantos.dev>
2025-08-20 19:04:46 +0200

    Add SeoSection

--- src/helpers/blogHelper.ts
+++ src/helpers/blogHelper.ts
@@ -5,3 +5,15 @@ export const getPosts = async () => await getCollection('blog')
 export const getPostSortedByDate = async () => (await getPosts()).sort(
     ({ data: { pubDate: pubDateA } },{ data: { pubDate: pubDateB } }) => pubDateB.valueOf() - pubDateA.valueOf(),
 )
+
+export const getSanitizedString = (data: string) =>
+    data.trim()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9\s-]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/^-|-$/g, '')
+
+export const getPostUrl = (title: string, site?: URL) => `${site}blog/${getSanitizedString(title)}`
